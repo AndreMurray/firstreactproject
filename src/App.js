53b8e6c691095a457105e4abe0957f4b0cd317f8
Andre Murray <slushymatis@gmail.com>
2023-08-13 23:41:55 -0500

    react forum building

--- src/App.js
+++ src/App.js
@@ -4,31 +4,36 @@ import {useState} from "react";
 
 function App() {
     const [newItem, setNewItem] = useState("")
+    const [items, setItems] = useState([])
+    function handleSubmit(e) {
+        e.preventDefault()
+        setItems(currentItems => {
+            return [...currentItems, {id: crypto.randomUUID(), title: newItem, completed: false},]
+    })
+        setNewItem("")
+    }
+
   return (
      <>
-   <form className="new-item-form">
+   <form className="new-item-form" onSubmit={handleSubmit}>
      <div className={"from-row"}>
        <label htmlFor={"item"}>New Item</label>
-         <input type="text" name="name" id={"item"} />
+         <input value={newItem} type="text" onChange={e => setNewItem(e.target.value)} name="name" id={"item"} />
      </div>
      <button className={"btn"}>Add Item</button>
    </form>
    <h1 className={"header"}>Todo List</h1>
    <ul className={"list"}>
-     <li>
-       <label>
-            <input type="checkbox" />
-           Item 1
-       </label>
-       <button className={"btn btn-danger"}>Delete</button>
-     </li>
-       <li>
-           <label>
-               <input type="checkbox" />
-               Item 2
-           </label>
-           <button className={"btn btn-danger"}>Delete</button>
-       </li>
+       {items.map(item => {
+           return <li key={item.id}>
+               <lable>
+                   <input type={"checkbox"} checked={item.completed} />
+                     {item.title}
+               </lable>
+               <button className={"btn btn-danger"}>Delete</button>
+           </li>
+       })}
+
    </ul>
     </>
   );
